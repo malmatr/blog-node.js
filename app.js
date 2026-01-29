5c98a27ccdb27e9a591b109df5fd9ca85e6eff9a
mallory matravers <matraversmallory@gmail.com>
2026-01-29 16:51:29 -0700

    final commit

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 require("dotenv").config();
 
+const cookieParser = require("cookie-parser");
+const MongoStore = require("connect-mongo");
 const expressLayouts = require("express-ejs-layouts");
 const express = require("express");
 const app = express();
 const PORT = 4000;
+
+const methodOverride = require("method-override");
+const session = require("express-session");
 
 const connectDB = require("./server/config/db");
 connectDB();
@@ -11,8 +16,22 @@ connectDB();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(cookieParser());
+app.use(methodOverride("_method"));
+
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGO_URI,
+    }),
+  }),
+);
+
 app.use(expressLayouts);
-app.use(express.status("public"));
+app.use(express.static("public"));
 app.set("layout", "./layouts/main");
 app.set("view engine", "ejs");
 
