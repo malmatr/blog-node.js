4c6216ef2f0ae0f3f72294f3037ca1e048c98542
mallory matravers <matraversmallory@gmail.com>
2026-01-28 19:11:11 -0700

    second commit

--- server/routes/main.js
+++ server/routes/main.js
@@ -11,8 +11,26 @@ router.get("/", async (req, res) => {
   };
 
   try {
-    const data = await Post.find().sort({ createdAt: "desc" });
+    const data = await Post.find().sort({ title: "desc" });
     res.render("index", { locals, data });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// Get Post by Id
+router.get("/post/:id", async (req, res) => {
+  try {
+    let slug = req.params.id;
+
+    const data = await Post.findById({ _id: slug });
+
+    const locals = {
+      title: data.title,
+      description:
+        "A Blog template application that will be used for your own use.",
+    };
+    res.render("post", { locals, data });
   } catch (error) {
     console.log(error);
   }
