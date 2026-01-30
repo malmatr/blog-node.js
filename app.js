3873c67dca70472367a3990730ba4a04e3d2b34e
mallory matravers <matraversmallory@gmail.com>
2026-01-29 17:23:31 -0700

    latest commit

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+const dns = require("node:dns");
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+const servers = dns.getServers();
+console.log("Node.js is using these DNS servers:", servers);
+
 require("dotenv").config();
 
 const cookieParser = require("cookie-parser");
